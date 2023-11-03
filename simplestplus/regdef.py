43b33858a729495d7ea6918fa6a9fb499a63c834
Amyr Francisco <amyrfrancisco11@gmail.com>
2023-11-03 16:09:02 +0800

    edited delim_indent

--- simplestplus/regdef.py
+++ simplestplus/regdef.py
@@ -5,7 +5,7 @@ definitions['digits'] = list('123456789')
 definitions['all_digits'] = definitions['digits'] + ['0']
 
 definitions['delim_space'] = definitions['all_alpha'] + ['\n', '"']
-definitions['delim_indent'] = definitions['all_alpha']
+definitions['delim_indent'] = definitions['all_alpha'] + ['\n']
 definitions['delim_newline'] = definitions['all_alpha'] + ['\n', ' ']
 definitions['delim_res_words'] = [' ', '\n']
 definitions['delim_word'] = definitions['delim_res_words']
