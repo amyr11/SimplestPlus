b28d97c8d9f3ff1605c7ac2b60069d6c8b454922
Amyr Francisco <amyrfrancisco11@gmail.com>
2023-11-10 15:18:02 +0800

    moved preprocess and verify_token inside tokenize method

--- src/lexer.py
+++ src/lexer.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from .errors import Error, LexicalError, InvalidIdentifier
+from .errors import Error, InvalidIdentifier, LexicalError
 from .machine import FinalState, MachineGroup, StateMachine
 from .tokens import Token, TokenType
 
@@ -485,6 +485,22 @@ class Lexer:
         self.code = code
 
     def tokenize(self, verbose=True) -> tuple[list[Token], list[Error]]:
+        def preprocess(code) -> str:
+            # Remove the excess spaces after each line before the newline
+            preprocessed_code = "\n".join([line.rstrip() for line in code.split("\n")])
+
+            return preprocessed_code
+
+        def verify_token(token) -> Optional[Error]:
+            # TODO: Other rules
+            if (
+                token.type == TokenType.IDENTIFIER
+                and token.val in TokenType.reserved_words.value
+            ):
+                return InvalidIdentifier(self.code, token)
+
+            return None
+
         def advance(val, row, col, cursor):
             if val == "\n":
                 row += 1
@@ -499,7 +515,7 @@ class Lexer:
         tokens = []
 
         # Preprocess
-        preprocessed_code = self._preprocess(self.code)
+        preprocessed_code = preprocess(self.code)
 
         # Tokenize code
         tmp_code = preprocessed_code
@@ -539,7 +555,7 @@ class Lexer:
             token.set_position(row, col)
 
             # Verify token according to rules
-            token_error = self._verify_token(token)
+            token_error = verify_token(token)
 
             if token_error:
                 if verbose:
@@ -553,19 +569,3 @@ class Lexer:
             tokens.append(token)
 
         return tokens, errors
-
-    def _preprocess(self, code) -> str:
-        # Remove the excess spaces after each line before the newline
-        preprocessed_code = "\n".join([line.rstrip() for line in code.split("\n")])
-
-        return preprocessed_code
-
-    def _verify_token(self, token) -> Optional[Error]:
-        # TODO: Other rules
-        if (
-            token.type == TokenType.IDENTIFIER
-            and token.val in TokenType.reserved_words.value
-        ):
-            return InvalidIdentifier(self.code, token)
-
-        return None
