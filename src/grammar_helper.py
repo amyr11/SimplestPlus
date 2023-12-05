74f4b0b4a0b357fb8000ea41f0073669606d6e2e
Amyr Francisco <amyrfrancisco11@gmail.com>
2023-12-06 02:16:38 +0800

    fixed cfg; fixed follow set algorithm; added follow set logs;

--- src/grammar_helper.py
+++ src/grammar_helper.py
@@ -12,7 +12,9 @@ class GrammarHelper:
         def get_first_set(production, cfg):
             first_set = set()
 
-            assert production in cfg.keys(), f"Production {production} not found in CFG."
+            assert (
+                production in cfg.keys()
+            ), f"Production {production} not found in CFG."
 
             for right_prod in cfg[production]:
                 first = right_prod[0]
@@ -36,11 +38,21 @@ class GrammarHelper:
     def _follow_set(self):
         all_follow_set_cache = {}
 
-        def get_follow_set(production, cfg, all_first_set):
-            all_follow_set_cache[production] = set()
+        def get_follow_set(production, cfg, all_first_set, from_production=None):
+            if from_production is not None:
+                tmp_log = f"Resolving pending {production} from {from_production}:\n"
+            else:
+                tmp_log = f"{production}:\n"
+            line = "-" * len(tmp_log) + "\n"
+            log = line + tmp_log + line
 
+            follow_set = set()
+            pending_follow_sets = []
+
+            production_row_no = 0
             for left, right in cfg.items():
                 for right_prod in right:
+                    production_row_no += 1
                     # Skip if current production is not found on the right side
                     if production not in right_prod:
                         continue
@@ -51,6 +63,8 @@ class GrammarHelper:
                         if item == production:
                             production_indeces.append(i)
 
+                    next_item = None
+
                     for production_index in production_indeces:
                         next_item_index = production_index + 1
                         resolved = False
@@ -60,45 +74,76 @@ class GrammarHelper:
                             next_item = right_prod[next_item_index]
 
                             if self._is_terminal(next_item):
-                                all_follow_set_cache[production].add(next_item)
+                                follow_set.add(next_item)
+                                log += f"From production {production_row_no}, added terminal `{next_item.value}` to {production}'s follow set ({production} -> {next_item.value}).\n"
                                 resolved = True
                             else:
                                 # Add the first set of the next non-terminal without the null
                                 next_first_set = all_first_set[next_item]
-
-                                all_follow_set_cache[production].update(
+                                follow_set.update(
                                     [
                                         item
                                         for item in next_first_set
                                         if item is not None
                                     ]
                                 )
+                                log += f"From production {production_row_no}, added {next_first_set} to {production}'s follow set ({production} -> FIRST({next_item})).\n"
 
                                 # If the first set of the next non-terminal contains null
                                 if None in next_first_set:
                                     next_item_index += 1
+                                    log += f"From production {production_row_no}, {next_item} contains null, moving to the next item.\n"
                                 else:
                                     resolved = True
 
-                    if not resolved:
-                        # Add to the follow set the follow set of the left production of the current production row
-                        if left in all_follow_set_cache.keys():
-                            all_follow_set_cache[production].update(
-                                all_follow_set_cache[left]
-                            )
-                        else:
-                            all_follow_set_cache[production].update(
-                                get_follow_set(left, cfg, all_first_set)
-                            )
+                        if not resolved and left != from_production:
+                            if next_item is None:
+                                log += f"From production {production_row_no}, {production} is the last item, adding {left}'s follow set to {production}'s follow set ({production} -> FOLLOW({left})).\n" 
+                            else:
+                                log += f"From production {production_row_no}, {next_item} is the last item, adding {left}'s follow set to {production}'s follow set ({production} -> FOLLOW({left})).\n"
+                            if left in all_follow_set_cache:
+                                # Add to the follow set the follow set of the left production of the current production row
+                                follow_set.update(all_follow_set_cache[left])
+                                log += f"Added {left}'s cached follow set {all_follow_set_cache[left]} to {production}'s follow set ({production} -> FOLLOW({left})).\n"
+                            else:
+                                # Add the left production to the pending follow sets
+                                if left not in pending_follow_sets:
+                                    pending_follow_sets.append(left)
+                                    log += f"Added {left} to pending follow sets.\n"
+                                else:
+                                    log += f"{left} is already in pending follow sets, skipping.\n"
+                        elif not resolved and left == from_production:
+                            log += f"From production {production_row_no}, {from_production} is not yet resolved, skipping.\n"
 
-            return all_follow_set_cache[production]
+            # Find the follow set of the pending productions
+            if len(pending_follow_sets) > 0:
+                log += f"Resolving pending follow sets from {production}: {pending_follow_sets}\n"
+            for pending_production in pending_follow_sets:
+                pending_follow_set, pending_log = get_follow_set(
+                    pending_production, cfg, all_first_set, production
+                )
+                follow_set.update(pending_follow_set)
+                log += pending_log
+                log += f"Added {pending_production}'s follow set {all_follow_set_cache[pending_production]} to {production}'s follow set ({production} -> FOLLOW({pending_production})).\n"
+
+            # Add the follow set of the current production to cache
+            all_follow_set_cache[production] = follow_set
+            log += f"Added {production}'s follow set {follow_set} to cache.\n"
+
+            return follow_set, log
 
         all_follow_set = {}
+        logs = ""
 
         for production in self._cfg.keys():
-            all_follow_set[production] = get_follow_set(
+            all_follow_set[production], log = get_follow_set(
                 production, self._cfg, self.first_set
             )
+            logs += log + "\n\n\n"
+
+        # write log to text file
+        with open("./dump/follow_set_log.txt", "w") as f:
+            f.write(logs)
 
         return all_follow_set
 
