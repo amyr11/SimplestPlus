093df65f355708fa02b185a237465c844db82743
amyr11 <amyrfrancisco11@gmail.com>
2024-02-19 19:10:19 +0800

    chore: remove commented code in test.py

--- test.py
+++ test.py
@@ -6,27 +6,6 @@ def print_no_file_found(path):
     print(f"{path} doesn't exist.")
     print()
 
-
-# def test_compile():
-#     file_path = sys.argv[1]
-
-#     if not os.path.exists(file_path):
-#         print_no_file_found(file_path)
-#         return
-
-#     with open(file_path, "r") as file:
-#         code = file.read()
-
-#     compiler = SimplestPlusCompiler(code)
-#     compiler.run()
-
-#     if compiler.errors:
-#         for error in compiler.errors:
-#             print(error.as_string(code))
-#     else:
-#         print(f"{file_path} Compiled successfully.")
-
-
 def test_compile():
     file_path = sys.argv[1]
 
